08891520d337614ccceb8b44b74a1216f3d0673b
almazuulu <almazuulu@gmail.com>
2022-12-04 04:09:31 +0600

    search functionality

--- carsell/cars/urls.py
+++ carsell/cars/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import cars, car_detail
+from .views import cars, car_detail, search
 
 urlpatterns = [
     path('', cars, name='cars'),
-    path('<str:id>', car_detail, name='car_detail')
+    path('search', search, name='search'),
+    path('<str:id>', car_detail, name='car_detail'),
+    
 ]
--- carsell/pages/views.py
+++ carsell/pages/views.py
@@ -6,13 +6,21 @@ from cars.models import Car, CarsPost
 def home(request):
     teams = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+    model_search = Car.objects.values_list('model', flat=True).distinct
+    city_search = Car.objects.values_list('city', flat=True).distinct
+    year_search = Car.objects.values_list('year', flat=True).distinct
+    bs_search = Car.objects.values_list('body_style', flat=True).distinct
+    
     all_cars = Car.objects.order_by('-created_date')
     # car_photos = featured_cars.carspost_set.all()
     context = {
         'teams': teams,
         'featured_cars': featured_cars,
         'all_cars': all_cars,
-        # 'car_photos': car_photos
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'bs_search': bs_search,
     }
     return render(request, 'pages/home.html', context)
 
