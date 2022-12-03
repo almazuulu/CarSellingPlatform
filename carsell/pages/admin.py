f5049350ed258a82e1c93b40da5d25eabda4bd97
almazuulu <almazuulu@gmail.com>
2022-12-03 21:16:03 +0600

    Car models, Admin, Car pics, Car list

--- carsell/pages/admin.py
+++ carsell/pages/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Team
+from django.utils.html import format_html
 
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        html_code = '<img src = "{}" width="40" style="border-radius: 50px;"/>'
+        return format_html(html_code.format(object.photo.url))
+    
+    search_fields = ('first_name', 'last_name', 'designation')
+    list_display = ('thumbnail','first_name', 'last_name', 'designation', 'created_date')
+    list_display_links = ('first_name', 'last_name', 'thumbnail')
+    list_filter = ('designation',)
 # Register your models here.
+admin.site.register(Team, TeamAdmin)
--- carsell/pages/models.py
+++ carsell/pages/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    facebook_link = models.URLField(max_length=255, blank=True)
+    twitter_link = models.URLField(max_length=255, blank=True)
+    google_link = models.URLField(max_length=255,blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
+    
+    class Meta:
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
