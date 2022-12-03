a6f16ee2328f7e0518767e7554a4bf15f6e5ee5d
almazuulu <almazuulu@gmail.com>
2022-12-03 16:44:13 +0600

    Cars app

--- carsell/pages/urls.py
+++ carsell/pages/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('about', about, name='about'),
+    path('services', services, name='services'),
+    path('contact', contact, name='contact' )
 ]
