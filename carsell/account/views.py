812b0a3dce109e1c9ef9e144721a05475b325c3a
Almazuulu <almazuulu@MacBook-Pro-Almazuulu.local>
2022-12-05 15:55:25 +0600

    Registration view and dashboard welcome message

--- carsell/account/views.py
+++ carsell/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def login(request):
@@ -10,8 +11,30 @@ def logout(request):
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Could not register')
-        return redirect('register')
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
+                messages.error(request, 'This user already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create(first_name=firstname, last_name=lastname,email=email, username=username, password=password)     
+                auth.login(request, user)
+                messages.success(request, 'You are now logged in')
+                return redirect('dashboard')
+                user.save()
+                messages.success(request, 'User has been successfully registered!')
+                return redirect('login') 
+        else:
+            messages.error(request, 'Password do not match')
+            return redirect('register')
+        # messages.error(request, 'Could not register')
+       
     else:
         return render(request, 'account/register.html')
 
